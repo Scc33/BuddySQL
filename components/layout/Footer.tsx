b0a7a833a721c7697e8681d52bed3093bf0f1542
Sean Coughlin <coughlinscc33@gmail.com>
2025-03-31 17:55:52 -0500

    Create link in footer on name

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,9 +3,11 @@ export default function Footer() {
     <footer className="bg-white">
       <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="mt-8 md:mt-0 md:order-1">
-          <p className="text-center text-sm text-gray-500">
-            &copy; {new Date().getFullYear()} Sean Coughlin
-          </p>
+          <a href="https://seancoughlin.me">
+            <span className="text-center text-sm text-gray-500">
+              &copy; {new Date().getFullYear()} Sean Coughlin
+            </span>
+          </a>
         </div>
         <div className="flex justify-center space-x-6 md:order-2">
           <a
