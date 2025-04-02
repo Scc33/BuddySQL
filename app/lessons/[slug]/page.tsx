838808ac47284a246c1166a59c3f01323e87ed93
Sean Coughlin <coughlinscc33@gmail.com>
2025-04-02 08:13:39 -0500

    Remove erroneous console log

--- app/lessons/[slug]/page.tsx
+++ app/lessons/[slug]/page.tsx
@@ -163,7 +163,6 @@ export default function LessonPage() {
 
   const previousLesson = getPreviousLesson(lesson.id);
   const nextLesson = getNextLesson(lesson.id);
-  console.log(lesson.challenge && " md:grid-cols-2");
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
       <div className="space-y-8">
